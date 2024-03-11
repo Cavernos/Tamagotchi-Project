97c76de4bd7e8e91d635a8dee2b10884b81e0f6d
Cavernos <louisdescavernes@gmail.com>
2024-03-11 19:50:33 +0100

    add Player class

--- Player.py
+++ Player.py
@@ -1,5 +1,16 @@
 class Player:
-    def __init__(self, biscuit):
-        self.biscuit = biscuit
+    """
 
+    """
+    def __init__(self, biscuit=50, name="PLayer"):
+        self._name = name
+        self._biscuit = biscuit
+
+    @property
+    def biscuit(self):
+        return self._biscuit
+
+    @biscuit.setter
+    def biscuit(self, value):
+        self._biscuit = value
 
