3c2cf37a669a5b001f003085009b340a7ab5db83
Nicolas <nico21.clement@gmail.com>
2024-03-12 21:26:45 +0100

    files update

---
+++ src/Player.py
@@ -0,0 +1,51 @@
+class Player:
+    """
+    A classed used to represent a Player
+    ...
+
+    Attributes
+    ----------
+    name : str
+        the name of the Player
+    biscuit : int
+        The number of biscuit in player inventory
+    """
+    def __init__(self, name="PLayer", biscuit=50) -> None:
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the Player
+        biscuit : int
+            The number of biscuit got by the Player
+        """
+        self._name: str = name
+        self._biscuit: int = biscuit
+
+    @property
+    def biscuit(self) -> int:
+        """
+        Biscuit Property equivalent of getter
+        """
+        return self._biscuit
+
+    @biscuit.setter
+    def biscuit(self, value) -> None:
+        """
+        Biscuit Setter
+        """
+        self._biscuit = value
+
+    @property
+    def name(self) -> str:
+        """
+        Name Property equivalent of getter
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value) -> None:
+        """
+        Name Setter
+        """
+        self._name = value
