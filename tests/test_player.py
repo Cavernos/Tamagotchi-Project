581c4c2adf6393c7e015cab943993b3303be2bb5
Nicolas <nico21.clement@gmail.com>
2024-03-12 23:36:26 +0100

    test_player : test feed

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,16 +1,23 @@
 import unittest
 from Player import Player
+from Tamagotchi import Tamagotchi
 
 
 class TestPlayer(unittest.TestCase):
     def setUp(self) -> None:
-        self.player = Player(name="PLayer", biscuit=50)
+        self.player = Player()
     
-    def test_is_instance_of_player(self):
+    def test_is_instance_of_player(self) -> None:
         self.assertIsInstance(self.player, Player)
 
-    def test_biscuit_is_greater_than_zero(self):
+    def test_biscuit_is_greater_than_zero(self) -> None:
         self.assertGreater(self.player.biscuit, 0)
+
+    def test_player_feed_tamagotchi(self) -> None:
+        tamagotchi = Tamagotchi()
+        tamagotchi.feed(self.player)
+
+
 
 
 if __name__ == '__main__':
