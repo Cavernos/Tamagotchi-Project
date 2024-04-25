9c216ec8837d0312a3eaba2e0dd65c372b822c8e
Cavernos <louisdescavernes@gmail.com>
2024-04-25 10:31:52 +0200

    Implement clock Class

--- src/clock.py
+++ src/clock.py
@@ -1,15 +1,28 @@
 import threading
-from tamagotchi import tamagotchi
+import tamagotchi
 from Player import Player
-class Clock(threading.Thread):
-    def __init__(self, name):
-        threading.Thread.__init__(name=name)
-        self.day_duration = 180
-        self.tamagotchis = tamagotchi
-        self.player = Player("Michel")
+import time
+DAY_DURATION = 10
 
-    def run(self):
+class Clock(threading.Thread):
+    def __init__(self, name) -> None:
+        threading.Thread.__init__(self, name=name)
+        self.day_duration = DAY_DURATION
+        self.tamagotchis = tamagotchi.tamagotchis
+
+    def run(self) -> None:
+        for element in self.tamagotchis:
+            tamagotchi.night_duration(element)
         while self.day_duration:
-            
+            for element in self.tamagotchis:
+                if tamagotchi.battle(element):
+                    tamagotchi.is_in_battle(self.tamagotchis)
+                if tamagotchi.die(element):
+                    return
+                if self.day_duration <= element["night_duration"]:
+                    tamagotchi.sleep_zzz(element)
+                tamagotchi.awake(element) 
+            time.sleep(1)       
             self.day_duration -= 1
 
+    
