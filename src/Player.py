e6d4ccd8f74b5340b746e39ddbd0e4a1f88ecd79
Nicolas <nico21.clement@gmail.com>
2024-04-25 09:21:13 +0200

    Player

--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,5 @@
+#--------------- Player ---------------#
+
 class Player:
     """
     A classed used to represent a Player
@@ -7,34 +9,39 @@ class Player:
     ----------
     name : str
         the name of the Player
-    biscuit : int
+    biscuits : int
         The number of biscuit in player inventory
     """
-    def __init__(self, name="PLayer", biscuit=50) -> None:
+
+    def __init___(self,
+                  name: str= "Player",
+                  biscuits: int= 50) -> None:
         """
         Parameters
         ----------
         name : str
             The name of the Player
-        biscuit : int
+        biscuits : int
             The number of biscuit got by the Player
         """
-        self._name: str = name
-        self._biscuit: int = biscuit
+        self.name: str = name
+        self.biscuits: int = biscuits
+        return None
 
     @property
-    def biscuit(self) -> int:
+    def biscuits(self) -> int:
         """
         Biscuit Property equivalent of getter
         """
-        return self._biscuit
+        return self._biscuits
 
-    @biscuit.setter
-    def biscuit(self, value) -> None:
+    @biscuits.setter
+    def biscuits(self, value) -> None:
         """
         Biscuit Setter
         """
-        self._biscuit = value
+        self._biscuits = value
+        return None
 
     @property
     def name(self) -> str:
@@ -49,10 +56,25 @@ class Player:
         Name Setter
         """
         self._name = value
+        return None
 
     # Methods
-    def give_biscuit(self):
+    def give_biscuit(self) -> None:
         """
         The Player feed the Tamagotchi
         """
-        self._biscuit -= 1
+        self._biscuits -= 1
+        eat()
+        return None
+
+    def play_with(self) -> None:
+        """
+        The Player feed the Tamagotchi
+        """
+        play()
+        return None
+
+#--------------- Test ---------------#
+
+if __name__ == "__main__":
+    ...
