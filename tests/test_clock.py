4b31de7d3d5b473abe8ddf08edb0cd66327206d6
Cavernos <louisdescavernes@gmail.com>
2024-04-30 22:42:42 +0200

    in according to issue #6, add test for tamagotchi

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -1,21 +1,10 @@
 import unittest
 import tamagotchi
-import time
 
 from clock import Clock
-from config import DAY_DURATION
 
 
 class TestClock(unittest.TestCase):
     def setUp(self):
         self.tamagotchis = tamagotchi.tamagotchis
         self.clock = Clock("test_clock")
-
-    def test_clock_duration(self):
-        self.clock.start()
-        duration = DAY_DURATION
-        while duration:
-            print(self.tamagotchis[0]['hunger'])
-            time.sleep(1)
-            duration -= 1
-        self.assertEqual(self.tamagotchis[0]['hunger'], 200 - 5 * DAY_DURATION)
