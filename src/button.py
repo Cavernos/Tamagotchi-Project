25af2cbfb55fa551a3787b8b87c62e89dbedc8c5
Nicolas <nico21.clement@gmail.com>
2024-05-18 03:16:55 +0200

    gui ça avance

--- src/button.py
+++ src/button.py
@@ -1,5 +1,24 @@
 # dev Nicolas
 
+# import
+import pygame
+
+pygame.init()
+
+
+# class Button
 class Button:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, screen, action, x, y, width, height, color=(255, 0, 0, 0)):
+        self.action = action
+        self.rect = pygame.Rect(x, y, width, height)
+        
+        # Créer une surface avec transparence pour le bouton
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.image.fill(color)
+        screen.screen.blit(self.image, self.rect.topleft)
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
--- src/gui.py
+++ src/gui.py
@@ -10,16 +10,19 @@ from threading import Event
 import pytmx.util_pygame
 from clock import Clock
 from models.player import Player
+from button import Button
 import tamagotchi
 
 
 class Gui:
     def __init__(self,
                  width: int=1024,
-                 height: int=576):
+                 height: int=576,
+                 zoom:int = 4):
         self.event = Event()
         self.clock = Clock("console_game", self.event)
         self.player = Player("John") 
+        self.zoom = zoom
 
         # screen
         self.screen = pygame.display.set_mode((width, height))
@@ -28,43 +31,57 @@ class Gui:
         pygame.display.set_icon(game_icon)
         
         self.pygame_clock = pygame.time.Clock()
-        self.pygame_clock.tick(30)  # limits FPS
+        self.pygame_clock.tick(30)
         
-        # map
-        self.map = pytmx.util_pygame.load_pygame('./assets/menu/menu.tmx')
-        self.map_data = pyscroll.data.TiledMapData(self.map)
-        self.map_layer = pyscroll.orthographic.BufferedRenderer(self.map_data, self.screen.get_size())
-        self.map_layer.zoom = 4
-
-        # layer
-        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=1)
-        
-
+       
     def run(self):
-        # main loop
+        self.menu()
         in_game = True
         while in_game:
-            self.group.draw(self.screen)
-            
 
-            
-            # pygame.QUIT
+            # pygame.event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     in_game = False
+                for button in self.buttons:
+                    if button.is_clicked(event):
+                        if button.action == "quit":
+                            pygame.quit()
+                        if button.action == "settings":
+                            self.menu()
+                        if button.action == "new_game":
+                            self.view_all_tamagotchis()
+
 
             pygame.display.flip()
         pygame.quit()
 
     def menu(self):
-        ...
-    
+        self.map = pytmx.util_pygame.load_pygame('./assets/menu/menu.tmx')
+        self.map_data = pyscroll.data.TiledMapData(self.map)
+        self.map_layer = pyscroll.orthographic.BufferedRenderer(self.map_data, self.screen.get_size())
+        self.map_layer.zoom = self.zoom
+
+        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=1)
+        self.group.draw(self.screen)
+
+        self.buttons = [Button(self, "new_game", self.zoom*92, self.zoom*87, self.zoom*74, self.zoom*7),
+                        Button(self, "quit", self.zoom*95, self.zoom*124, self.zoom*66, self.zoom*7)]
+
     def setting(self):
         ...
     
     def view_all_tamagotchis(self):
-        ...
-    
+        self.map = pytmx.util_pygame.load_pygame('./assets/map_maison/map_maison.tmx')
+        self.map_data = pyscroll.data.TiledMapData(self.map)
+        self.map_layer = pyscroll.orthographic.BufferedRenderer(self.map_data, self.screen.get_size())
+        self.map_layer.zoom = self.zoom
+
+        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=1)
+        self.group.draw(self.screen)
+
+        self.buttons = [Button(self, "settings", 0, 0, self.zoom*16, self.zoom*16, (70, 70, 70, 128))]
+
     def view_tamagotchi(self, tamagotchi):
         ...
 
