46c919a7c8787da841738ef4a4f6fccbbc283144
Cavernos <louisdescavernes@gmail.com>
2024-05-18 20:41:47 +0200

    Test to resolve event problem

--- src/tamagoatchi/app/gui/views/MenuView.py
+++ src/tamagoatchi/app/gui/views/MenuView.py
@@ -13,10 +13,20 @@ class MenuView(GUIView):
         super().__init__(screen, ext_dict)
         map = pytmx.util_pygame.load_pygame(self.view_location)
         zoom = screen.get_size()[0] / MAP_SIZE[0]
-        self.buttons = [Button(screen, self.new_game, zoom * 92, zoom * 87, zoom * 74, zoom * 7)]
+        self.buttons = [Button(screen, self.new_game, zoom * 92, zoom * 87, zoom * 74, zoom * 7),
+                        Button(screen, self.exit, zoom*95, zoom*124, zoom*66, zoom*7)]
+        for button in self.buttons:
+            button.draw()
         map_data = pyscroll.TiledMapData(map)
         self.map_layer = pyscroll.orthographic.BufferedRenderer(map_data, pygame.display.get_window_size())
         self.map_layer.zoom = zoom
 
     def new_game(self):
         self.redirect('home')
+
+    @staticmethod
+    def exit():
+        pygame.quit()
+        exit(0)
+
+
--- src/tamagoatchi/lib/widgets/Button.py
+++ src/tamagoatchi/lib/widgets/Button.py
@@ -1,4 +1,5 @@
 # dev Nicolas
+import types
 
 # import
 import pygame
@@ -10,22 +11,32 @@ from tamagoatchi.lib.event import EventHandler
 class Button:
     def __init__(self, screen, action, x, y, width, height, color=(255, 0, 0, 128)):
         self.action = action
+        self.color = color
         self.rect = pygame.Rect(x, y, width, height)
         self.screen = screen
         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
-        self.image.fill(color)  # (r, g, b, transparency)
+
+    def draw(self):
+        self.image.fill(self.color)  # (r, g, b, transparency)
         self.screen.blit(self.image, self.rect.topleft)
 
     @EventHandler.register(pygame.MOUSEBUTTONDOWN)
     def on_clicked(self, event):
         if self.rect.collidepoint(event.pos):
-            self.action()
+            if isinstance(self.action, types.MethodType) or isinstance(self.action, types.FunctionType):
+                self.action()
 
     @EventHandler.register(pygame.MOUSEMOTION)
     def on_hover(self, event):
         x, y = event.pos
-        if (x in range(self.rect.x, self.rect.x + self.rect.width)) and (y in range(self.rect.y, self.rect.y + self.rect.height)):
+        if (x in range(self.rect.x, self.rect.x + self.rect.width)) and (
+                y in range(self.rect.y, self.rect.y + self.rect.height)):
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
         else:
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
+    @staticmethod
+    def destroy():
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        EventHandler.unregister(pygame.MOUSEMOTION)
+        EventHandler.unregister(pygame.MOUSEBUTTONDOWN)
