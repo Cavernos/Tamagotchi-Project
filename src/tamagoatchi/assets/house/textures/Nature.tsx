c552b130e32f74ba8b69475ffbd059528a51d487
Cavernos <louisdescavernes@gmail.com>
2024-05-20 00:02:52 +0200

    Repair house_view

--- src/tamagoatchi/assets/house/textures/Nature.tsx
+++ src/tamagoatchi/assets/house/textures/Nature.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Nature" tilewidth="16" tileheight="16" tilecount="78" columns="13">
- <image source="Nature.png" trans="37994b" width="208" height="96"/>
+<tileset version="1.10" tiledversion="1.10.2" name="nature" tilewidth="16" tileheight="16" tilecount="78" columns="13">
+ <image source="nature.png" width="208" height="96"/>
 </tileset>
