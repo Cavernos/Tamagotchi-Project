0d95bf742ac222c3fc3af5e6cb603ebf1d20e1f3
Cavernos <louisdescavernes@gmail.com>
2024-05-23 10:09:57 +0200

    update gitignore

--- src/tamagoatchi/assets/map_maison/textures/objects_demo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="objects_demo" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="objects_demo.png" trans="37994b" width="512" height="512"/>
-</tileset>
