40fd00942d82550da04884972f064af55e591de6
Nicolas <nico21.clement@gmail.com>
2024-05-27 12:01:22 +0200

    coucou

--- src/button.py
+++ src/button.py
@@ -6,7 +6,7 @@ import pygame
 
 # class Button
 class Button:
-    def __init__(self, game, obj, action, color=(255, 0, 0, 0)):
+    def __init__(self, game, obj, action, color=(255, 0, 0, 128)):
         self.action = action
         self.rect = pygame.Rect(obj.x*game.map_layer.zoom,
                                 obj.y*game.map_layer.zoom,
@@ -16,10 +16,6 @@ class Button:
         self.image = pygame.Surface((obj.width*game.map_layer.zoom, obj.height*game.map_layer.zoom), pygame.SRCALPHA)
         self.image.fill(color)  # (r, g, b, transparency)
         game.screen.blit(self.image, self.rect.topleft)
-
-    def do_action(self):
-        ...
-
 
     def is_clicked(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
